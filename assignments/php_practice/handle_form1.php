ebd21e20fe143e1b1e8150ced7367c821c2ef1c2
RyHutch <rhutchins42@gmail.com>
2017-02-22 20:58:46 -0500

    PHP Forms

---
+++ assignments/php_practice/handle_form1.php
@@ -0,0 +1,15 @@
+
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+<head>
+    <meta http-equiv="Content-Type"
+          content="text/html; charset=UTF-8">
+    <title>Handle Form 1</title>
+</head>
+<body>
+<?php
+$text = $_POST['text'];
+print "<p>Hello $text</p>";
+?>
+</body>
+</html>
---
+++ assignments/php_practice/handle_form2.php
@@ -0,0 +1,17 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+<head>
+    <meta http-equiv="Content-Type"
+          content="text/html; charset=UTF-8">
+    <title>Handle Form 2</title>
+</head>
+<body>
+<?php
+$title = $_POST['title'];
+$forename = $_POST['forename'];
+$surname = $_POST['surname'];
+$address = $_POST['address'];
+print '<p>Hello, ' . "$title $forename $surname" . ' of ' . "$address.</p>";
+?>
+</body>
+</html>
